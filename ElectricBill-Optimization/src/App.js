a2add60774207cea7dfa92a7352898738cc09de0
Utkarsh Saxena <nive54@gmail.com>
2022-09-18 16:04:33 +0530

    i

---
+++ ElectricBill-Optimization/src/App.js
@@ -0,0 +1,32 @@
+import { render } from "react-dom";
+import './style.css'
+import { StrictMode } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+//import SelectAppliances from "./components/SelectAppliances";
+import ShowAppliances from "./components/ShowAppliances";
+import Type from "./components/Type";
+//import ShowAppliances from "./components/ShowAppliances";
+
+
+const App = () => {
+  return (
+    <StrictMode>
+      
+        <BrowserRouter>
+          
+          <Routes>
+            {/* <Route path="/details/:id" element={<Details />} /> */}
+            <Route path="/" element={<Home />} />
+            <Route path={"/selectappliances"} element={<ShowAppliances />} />
+            <Route path={"/type"} element={<Type />} />
+            <Route path={"/show"} element={<ShowAppliances />} />
+
+          </Routes>
+        </BrowserRouter>
+  
+    </StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
